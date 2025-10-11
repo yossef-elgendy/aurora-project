788953f81e161fb73edb1d866d958af0d23a5d0b
yossef-elgendy <yossef.elgendy55@gmail.com>
2025-10-11 11:25:15 +0300

    Displayed the new field within the address details

---
+++ src/app/code/Escooter/Checkout/view/frontend/requirejs-config.js
@@ -0,0 +1,8 @@
+var config = {
+    map: {
+        '*': {
+            'Magento_Checkout/template/billing-address/details.html':
+                'Escooter_Checkout/template/billing-address/details.html'
+        }
+    }
+};
